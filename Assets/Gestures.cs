b06fcb803371a4158f65123bf5475af60497749d
bitinvert <toxitygamer@gmail.com>
2015-05-15 13:01:25 +0200

    deal with it

--- Assets/Gestures.cs
+++ Assets/Gestures.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class Gestures : MonoBehaviour {
 
+	public int pIntCMSpeed = 50;
+
 	// Use this for initialization
 	void Start () {
 	
 		var mTKRecPan = new TKPanRecognizer();
 
+		mTKRecPan.gestureRecognizedEvent += (r) => 
+		{
+			Camera.main.transform.position -= new Vector3( mTKRecPan.deltaTranslation.x, 0, mTKRecPan.deltaTranslation.y ) / pIntCMSpeed;
+		};
+		TouchKit.addGestureRecognizer(mTKRecPan);
 	}
 }
