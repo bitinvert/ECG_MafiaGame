7b621155b932210f79afad8a4a9318446071aaee
svmagnus <magnussen.sven@yahoo.de>
2015-05-22 12:57:54 +0200

    First primitive implimentation of playing field with prefabs for wall, floor and cover
    Very abstract. Much primitive. So much wow!

---
+++ Assets/Scripts/PlayField/Field.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Field : MonoBehaviour {
+	public bool pBoolCanBeEntered;
+	public int pIntCoverValue;
+	public bool pBooleanIsBlockingView;
+}
