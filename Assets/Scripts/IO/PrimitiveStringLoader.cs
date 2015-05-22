a60c17add9ba132365a72dce9458eccadc8a0aaa
svmagnus <magnussen.sven@yahoo.de>
2015-05-22 16:36:22 +0200

    Added Comments

--- Assets/Scripts/IO/PrimitiveStringLoader.cs
+++ Assets/Scripts/IO/PrimitiveStringLoader.cs
@@ -5,7 +5,12 @@ using System.IO;
 using System.Collections.Generic;
 
 public class PrimitiveStringLoader : MonoBehaviour {
-	
+	/*
+	 * Static method to read text out of a file 
+	 * Author: Sven Magnussen
+	 * <para>String mStringPath The Path to the textfile </para>
+	 * <returns> List &lt; string &gt Returns a list. String elements of this list contain a single line of the text </returns>
+	 * */
 
 	public static List<String> LoadString(string mStringPath){
 		//Setup the file reader
--- Assets/Scripts/PlayField/PrimitveLevelLoader.cs
+++ Assets/Scripts/PlayField/PrimitveLevelLoader.cs
@@ -18,28 +18,37 @@ public class PrimitveLevelLoader : MonoBehaviour {
 		List<string> mLstStringLevel = PrimitiveStringLoader.LoadString(mStringPath);
 		CreateLevel (mLstStringLevel);
 	}
-
+	/*
+	 * Creates the level in the scene out of string List, containg tags for the differnet field types. 
+	 * Author: Sven Magnussen
+	 * <para>List &lt; string &gt;  mLstString -> a list containing the single level file lines to create the level step by step </para>
+	 * 
+	 * */
 	private void CreateLevel(List<string> mLstStringLevel){
+		GameObject mGORoot = Instantiate(new GameObject());
+		mGORoot.name= "Level";
 		for (int mIntXAxis = 0; mIntXAxis < mLstStringLevel.Count; mIntXAxis = mIntXAxis +1) {
-			CreateLine(mLstStringLevel[mIntXAxis],mIntXAxis);
+			CreateLine(mLstStringLevel[mIntXAxis],mIntXAxis,mGORoot);
 		}
 	}
 
-	private void CreateLine(string mStringLine, int mIntXPos){
+	private void CreateLine(string mStringLine, int mIntXPos,GameObject mGORoot){
 		char[] mAryCharFields = mStringLine.ToCharArray ();
 		for(int mIntZAxis = 0;mIntZAxis<mAryCharFields.Length;mIntZAxis= mIntZAxis+1) {
+			GameObject mGOField=null;
 			switch(mAryCharFields[mIntZAxis]){
 			case 'F' : 
-				Instantiate(pGOFloor,new Vector3(mIntXPos,1,mIntZAxis),new Quaternion());
+				mGOField = (GameObject)Instantiate(pGOFloor,new Vector3(mIntXPos,1,mIntZAxis),new Quaternion());
 				break;
 			case 'W':
-				Instantiate(pGOWall,new Vector3(mIntXPos,1,mIntZAxis),new Quaternion());
+				mGOField = (GameObject)Instantiate(pGOWall,new Vector3(mIntXPos,1,mIntZAxis),new Quaternion());
 				break;
 			case 'C':
-				Instantiate(pGOCover,new Vector3(mIntXPos,1,mIntZAxis),new Quaternion());
+				mGOField = (GameObject)Instantiate(pGOCover,new Vector3(mIntXPos,1,mIntZAxis),new Quaternion());
 				break;
 
 			}
+			mGOField.transform.parent=mGORoot.transform;
 		}
 
 	}
