b69844a7c8e5acda00a19dd39eea7792340b022c
DarknessXShiro <darknessxshiro@gmail.com>
2015-05-24 00:48:36 +0200

    networking sample added (ptp)
    networking test, ptp model used;
    asynchron test: use two builds, one has
    to play server (start server
    button) and other one joins server (refresh
    hostlist button then
    roomname button)

---
+++ Assets/Scripts/Networking/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float speed = 10f;
+	private Rigidbody rigidbody;
+	private NetworkView networkView;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start ()
+	{
+		rigidbody = GetComponent <Rigidbody> ();
+		networkView = GetComponent <NetworkView> ();
+	}
+
+	/// <summary>
+	/// Fixeds the update.
+	/// </summary>
+	void FixedUpdate ()
+	{
+		if (networkView.isMine) 
+		{
+			InputMovement ();
+		}
+	}
+
+	/// <summary>
+	/// Test movement via wasd-keys.
+	/// </summary>
+	void InputMovement ()
+	{
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			rigidbody.MovePosition (rigidbody.position + Vector3.right * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			rigidbody.MovePosition (rigidbody.position + Vector3.left * speed * Time.deltaTime);
+		}
+	}
+}
