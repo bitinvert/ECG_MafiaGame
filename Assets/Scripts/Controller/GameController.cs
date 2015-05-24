b57edbecc2cb70bd643aa60e30313b65d5f3b094
bitinvert <toxitygamer@gmail.com>
2015-05-24 09:51:57 +0200

    Taping a sprite and a tile
    Saving the tapped tile and the tapped sprite

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -3,8 +3,36 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 	// Use this for initialization
-	void Start () {
+	public GameObject mGOPlayableSprite;
+	public GameObject mGOTileToMove;
 
+	void Start () {
+		var mTKRecCharTap = new TKTapRecognizer();
+		mTKRecCharTap.gestureRecognizedEvent += (r) =>
+		{
+
+
+			Vector3 mVec3TapPos = new Vector3(mTKRecCharTap.touchLocation().x,
+			                                  mTKRecCharTap.touchLocation().y,
+			                                  0f);
+			RaycastHit mRHInfo = new RaycastHit();
+			if(!mGOPlayableSprite && Physics.Raycast(Camera.main.ScreenPointToRay(mVec3TapPos), out mRHInfo))
+			{
+				if(mRHInfo.collider.gameObject.tag == "Playable")
+				{
+					mGOPlayableSprite = mRHInfo.collider.gameObject;
+				}
+			}
+			else if(mGOPlayableSprite && Physics.Raycast(Camera.main.ScreenPointToRay(mVec3TapPos), out mRHInfo))
+			{
+				if(mRHInfo.collider.name == "FieldFloor(Clone)" ||
+				   mRHInfo.collider.name == "FieldCover(Clone)")
+				{
+					mGOTileToMove = mRHInfo.collider.gameObject;
+				}
+			}
+		};
+		TouchKit.addGestureRecognizer(mTKRecCharTap);
 	}
 	
 	// Update is called once per frame
