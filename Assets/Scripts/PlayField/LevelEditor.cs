a7a0ed63cb124ba6e82a96da2d638f8b09f71c15
svmagnus <magnussen.sven@yahoo.de>
2015-06-05 14:38:29 +0200

    Simple LevelEditor Scene

---
+++ Assets/Scripts/PlayField/LevelEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelEditor : MonoBehaviour {
+	public bool pBoolLoadOnStart;
+	public bool pBoolSaveOnClose;
+	public LevelController pLevelController;
+	// Use this for initialization
+	void Start () {
+		if (pBoolLoadOnStart) {
+			pLevelController.LoadLevel ("assets/safedLevel"); //Load Level for fields and solve problem with xml merge mybe 2 files?
+			Debug.Log ("Load");	
+		}
+	}
+	
+	// Update is called once per frame
+	void OnApplicationQuit() {
+		if (pBoolSaveOnClose){
+			pLevelController.SaveLevel ("assets/safedLevel");
+			Debug.Log("Save");
+		}
+	}
+}
