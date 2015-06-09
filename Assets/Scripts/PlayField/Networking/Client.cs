82bb2b438175376a1e8868559bd0e978a035f203
bitinvert <toxitygamer@gmail.com>
2015-06-09 14:48:01 +0200

    Revert "replaced scripts"
    This reverts commit 6f025bdc6be88fb4daf22cdb6e3714aaea811279.

---
+++ Assets/Scripts/PlayField/Networking/Client.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Client : MonoBehaviour {
+
+	string remoteIP = "127.0.0.1";
+	int remotePort = 25000;
+	NetworkView networkView;
+	//List<string> missions;
+	//bool missionOn = false;
+	public bool loggedIn = false;
+	string username;
+	string password;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Awake ()
+	{
+		networkView = GetComponent <NetworkView> ();
+		//missions = new List<string> ();
+		connectToServer ();
+		DontDestroyOnLoad (this);
+		Application.LoadLevel ("MainMenu");
+	}
+
+	/// <summary>
+	/// Connects to server.
+	/// </summary>
+	void connectToServer() {
+		Network.Connect(remoteIP, remotePort);
+	}
+
+	void OnConnectedToServer() {
+		Debug.Log("Connected to server");
+	}
+
+	/// <summary>
+	/// Disconnects from server.
+	/// </summary>
+	public void Logout() {
+		//Network.Disconnect();
+		//missionOn = !missionOn;
+		//missions.Clear ();
+		loggedIn = false;
+	}
+
+	/// <summary>
+	/// Client sends registration-request to server.
+	/// </summary>
+	/// <param name="username">Username.</param>
+	/// <param name="password">Password.</param>
+	[RPC]
+	public void RequestRegistration(string username, string password){
+		networkView.RPC ("RegistrateUser", RPCMode.Server, username, password, Network.player);
+	}
+
+	/// <summary>
+	/// Client sends login-request to server.
+	/// </summary>
+	/// <param name="username">Username.</param>
+	/// <param name="password">Password.</param>
+	[RPC]
+	public void RequestLogin(string username, string password)
+	{
+		networkView.RPC ("AuthorizeLogin", RPCMode.Server, username, password, Network.player);
+	}
+	
+	/// <summary>
+	/// Login Client.
+	/// </summary>
+	/// <param name="authorized">If set to <c>true</c> authorized.</param>
+	[RPC]
+	void Login(bool authorized)
+	{
+		if (authorized) {
+			loggedIn = true;
+		} else {
+			Logout ();
+		}
+	}
+
+	/* Not used yet
+	/// <summary>
+	/// Request missions from server.
+	/// </summary>
+	[RPC]
+	void RequestMissions()
+	{
+		networkView.RPC ("GetMissions", RPCMode.Server, Network.player);
+	}
+
+	/// <summary>
+	/// Update missions list with received string.
+	/// </summary>
+	/// <param name="missionsString">Missions string.</param>
+	[RPC]
+	void ShowMissions(string missionsString)
+	{
+		string [] missionArray = missionsString.Split (new char[] {' '});
+		missions.Clear ();
+		foreach (string s in missionArray) 
+		{
+			missions.Add (s);
+		}
+		missionOn = true;
+	}
+
+	[RPC]
+	void RequestMap()
+	{
+		networkView.RPC ("BuildMap", RPCMode.Server);
+	}
+	*/
+
+	//Server RPC functions
+	[RPC]
+	void RegistrateUser (string username, string password, NetworkPlayer player)
+	{
+	}
+
+	[RPC]
+	void AuthorizeLogin (string username, string password, NetworkPlayer player)
+	{
+	}
+	/* Not used yet
+	[RPC]
+	void GetMissions (NetworkPlayer sender)
+	{
+		//empty
+	}
+
+	[RPC]
+	void BuildMap ()
+	{
+
+	}
+	*/
+
+
+}
---
+++ Assets/Scripts/PlayField/Networking/Server.cs
@@ -0,0 +1,231 @@
+﻿using UnityEngine;
+using System;
+using Mono.Data.Sqlite;
+using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Server : MonoBehaviour {
+	
+	int listenPort = 25000;
+	int maxClients = 2;
+	IDbConnection dbconn;
+	IDbCommand dbcmd;
+	
+	NetworkView networkView;
+	
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start ()
+	{
+		networkView = GetComponent <NetworkView> ();
+	}
+	
+	/// <summary>
+	/// Starts the server. First initialize Server on port, then build connection to a database.
+	/// </summary>
+	private void startServer() {
+		Network.InitializeServer(maxClients, listenPort, false);
+		string conn = "URI=file:" + Application.dataPath + "/ServerDatabase.s3db"; 
+		dbconn = new SqliteConnection(conn);
+		dbconn.Open(); 
+	}
+	
+	/// <summary>
+	/// Shut down server and close database connection.
+	/// </summary>
+	private void stopServer() {
+		Network.Disconnect();
+		
+		dbconn.Close();
+		dbconn = null;
+	}
+	
+	/// <summary>
+	/// Manage view of GUI.
+	/// </summary>
+	void OnGUI ()
+	{
+		if (Network.peerType == NetworkPeerType.Disconnected) {
+			GUILayout.Label("Network server is not running.");
+			if (GUILayout.Button ("Start Server"))
+			{				
+				startServer();	
+			}
+		}
+		else {
+			if (Network.peerType == NetworkPeerType.Connecting)
+				GUILayout.Label("Network server is starting up...");
+			else { 
+				GUILayout.Label("Network server is running.");        	
+				showServerInformation(); 	
+				showClientInformation();
+			}
+			if (GUILayout.Button ("Stop Server"))
+			{				
+				stopServer();	
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Shows the client information.
+	/// </summary>
+	private void showClientInformation() {
+		GUILayout.Label("Clients: " + Network.connections.Length + "/" + maxClients);
+		foreach(NetworkPlayer p in Network.connections) {
+			GUILayout.Label(" Player from ip/port: " + p.ipAddress + "/" + p.port);	
+		}
+	}
+	
+	/// <summary>
+	/// Shows the server information.
+	/// </summary>
+	private void showServerInformation() {
+		GUILayout.Label("IP: " + Network.player.ipAddress + " Port: " + Network.player.port);  
+	}
+	
+	/// <summary>
+	/// Executes client's registration request.
+	/// </summary>
+	/// <param name="username">Username.</param>
+	/// <param name="password">Password.</param>
+	/// <param name="sender">Sender.</param>
+	[RPC]
+	void RegistrateUser(string username, string password, NetworkPlayer sender)
+	{
+		dbcmd = dbconn.CreateCommand ();
+		IDbDataParameter usernameParam = dbcmd.CreateParameter ();
+		IDbDataParameter passwordParam = dbcmd.CreateParameter ();
+		dbcmd.CommandText = "INSERT INTO Player VALUES(@username, @password);";
+		
+		usernameParam.DbType = DbType.String;
+		usernameParam.Value = username;
+		usernameParam.ParameterName = "username";
+		
+		passwordParam.DbType = DbType.String;
+		passwordParam.Value = password;
+		passwordParam.ParameterName = "password";
+		
+		dbcmd.Parameters.Add (usernameParam);
+		dbcmd.Parameters.Add (passwordParam);
+		
+		if (dbcmd.ExecuteNonQuery () == 1) 
+		{
+			networkView.RPC ("Login", sender, true);
+		} else {
+			networkView.RPC ("Login", sender, false);
+		}
+		
+		dbcmd.Dispose();
+		dbcmd = null;
+		
+	}
+	
+	/// <summary>
+	/// Authorizes client's login request.
+	/// </summary>
+	/// <param name="username">Username.</param>
+	/// <param name="password">Password.</param>
+	/// <param name="sender">Sender.</param>
+	[RPC]
+	void AuthorizeLogin(string username, string password, NetworkPlayer sender)
+	{
+		dbcmd = dbconn.CreateCommand ();
+		IDbDataParameter usernameParam = dbcmd.CreateParameter ();
+		IDbDataParameter passwordParam = dbcmd.CreateParameter ();
+		dbcmd.CommandText = "SELECT * FROM Player WHERE username==@username AND password==@password;";
+		
+		usernameParam.DbType = DbType.String;
+		usernameParam.Value = username;
+		usernameParam.ParameterName = "username";
+		
+		passwordParam.DbType = DbType.String;
+		passwordParam.Value = password;
+		passwordParam.ParameterName = "password";
+		
+		dbcmd.Parameters.Add (usernameParam);
+		dbcmd.Parameters.Add (passwordParam);
+		
+		IDataReader reader = dbcmd.ExecuteReader ();
+		
+		if (reader.Read ()) 
+		{
+			networkView.RPC("Login", sender, true);
+		} else {
+			networkView.RPC("Login", sender, false);
+		}
+		
+		reader.Close ();
+		dbcmd.Dispose();
+		dbcmd = null;
+	}
+	
+	/*
+	[RPC]
+	void BuildMap()
+	{
+		//generate map
+
+	}
+
+	/// <summary>
+	/// Get missions from database and send them to clients.
+	/// </summary>
+	[RPC]
+	void GetMissions(NetworkPlayer sender)
+	{
+		dbcmd = dbconn.CreateCommand ();
+		dbcmd.CommandText = "SELECT * FROM Missions;";
+		IDataReader reader = dbcmd.ExecuteReader ();
+		string missions = "";
+
+		while (reader.Read()) 
+		{
+			missions += reader.GetString (0) + " ";
+		}
+		missions = missions.Trim ();
+		//rpcmode nur an aufrufer zurueck
+		networkView.RPC ("ShowMissions", sender, missions);
+
+		reader.Close ();
+		dbcmd.Dispose();
+		dbcmd = null;
+	}
+	*/
+	
+	//RPC client functions
+	[RPC]
+	public void RequestRegistration(string username, string password)
+	{
+	}
+	
+	[RPC]
+	void CheckLogin ()
+	{
+	}
+	
+	[RPC]
+	void Login (bool authorized)
+	{
+	}
+	/*
+	[RPC]
+	void RequestMissions()
+	{
+		//empty
+	}
+
+	[RPC]
+	void ShowMissions(string missions)
+	{
+		//empty
+	}
+
+	[RPC]
+	void RequestMap ()
+	{
+	}
+	*/
+}
