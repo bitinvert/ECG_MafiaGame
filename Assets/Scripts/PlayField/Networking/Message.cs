17473b5fc88db569a05fd5d51eab72572aaeb72d
bitinvert <toxitygamer@gmail.com>
2015-06-11 12:22:01 +0200

    Start of message system

---
+++ Assets/Scripts/PlayField/Networking/Message.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Message{
+
+	string mStringTimeStamp;
+	string mStringMap;
+	string mStringPlayName;
+	string mStringEnemyName;
+	ActionType mATActionPerformed;
+	GameObject[] mGOCharactersInvolved;
+	float[] mFloatDamadge;
+	Vector3[] mVec3Movement;
+
+	public string pack()
+	{
+		return "NULL";
+	}
+
+	public void unpack(string message)
+	{
+
+	}
+
+	enum ActionType
+	{
+		Movement,
+		Attack,
+		Other //More are coming
+	}
+}
