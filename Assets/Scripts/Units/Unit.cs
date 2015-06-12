1103f4fb5263a4d2fa31bc29ae6abf125343f4f2
svmagnus <magnussen.sven@yahoo.de>
2015-06-12 14:03:21 +0200

    Insert Demoscene and some ArtAssets

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -5,6 +5,7 @@ public class Unit : MonoBehaviour {
 	public string pStringName;
 	public int pIntAttack;
 	public int pIntDefense;
+	public int pIntHealth;
 	public int pIntMoveDistance;
 	//inventory is missing
 }
