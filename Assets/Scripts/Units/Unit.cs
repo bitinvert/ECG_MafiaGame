c4ca53d070cd456a344b41f664fe583cba5d47e6
svmagnus <magnussen.sven@yahoo.de>
2015-06-12 13:06:45 +0200

    implemented unit script

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+	public string pStringName;
+	public int pIntAttack;
+	public int pIntDefense;
+	public int pIntMoveDistance;
+	//inventory is missing
+}
