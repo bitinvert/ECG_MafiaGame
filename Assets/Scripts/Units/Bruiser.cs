aa57e1bfe581a7cba5abfca1865c9d6eaf9be2f8
svmagnus <magnussen.sven@yahoo.de>
2015-06-26 15:14:31 +0200

    Medic Special

--- Assets/Scripts/Units/Bruiser.cs
+++ Assets/Scripts/Units/Bruiser.cs
@@ -5,13 +5,16 @@ using System.Collections.Generic;
 public class Bruiser : Unit {
 
 	public void UseSpecial(Unit mUnitOther){
-		Vector3 mPosTargetPosition = mUnitOther.gameObject.transform.position;
-		List<Node> mListNeighbours = pFFWalkArea.pGridField.GetNeighbours(pFFWalkArea.pGridField.NodeFromWorldPosition(mUnitOther.gameObject.transform.position-mUnitOther.mVec3Offset));
-		foreach (Node mNodeNeighbour in mListNeighbours) {
-			if (mNodeNeighbour.pBoolWalkable){
-				mUnitOther.gameObject.transform.position = Vector3.MoveTowards (mUnitOther.gameObject.transform.position,(mNodeNeighbour.pVec3WorldPos+mUnitOther.mVec3Offset),mUnitOther.pFloatSpeed*Time.deltaTime);
-
-				break;
+		if (mUnitOther.pBoolEnemy) {
+			List<Node> mListNeighbours = pFFWalkArea.pGridField.GetNeighbours (pFFWalkArea.pGridField.NodeFromWorldPosition (mUnitOther.gameObject.transform.position - mUnitOther.mVec3Offset));
+			foreach (Node mNodeNeighbour in mListNeighbours) {
+				if (mNodeNeighbour.pBoolWalkable) {
+					Vector3 mPosTargetPosition = mUnitOther.gameObject.transform.position;
+					pAStarPathfinding.FindPath (this.gameObject.transform.position - this.mVec3Offset, mPosTargetPosition - mUnitOther.mVec3Offset);
+					this.move ();
+					mUnitOther.gameObject.transform.position = Vector3.MoveTowards (mUnitOther.gameObject.transform.position, (mNodeNeighbour.pVec3WorldPos + mUnitOther.mVec3Offset), mUnitOther.pFloatSpeed * Time.deltaTime);
+					break;
+				}
 			}
 		}
 	}
--- Assets/Scripts/Units/Medic.cs
+++ Assets/Scripts/Units/Medic.cs
@@ -2,5 +2,12 @@
 using System.Collections;
 
 public class Medic : Unit {
-	
+	public void UseSpecial(Unit mUnitOther){
+		if (mUnitOther.pBoolEnemy) {
+			mUnitOther.pIntHealth = mUnitOther.pIntHealth + 4;
+			if (mUnitOther.pIntHealth>12){
+				mUnitOther.pIntHealth = 12;
+			}
+		}
+	}
 }
