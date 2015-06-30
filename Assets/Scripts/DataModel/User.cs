a2e969459736dad65af9a34dc16d83c6c2fe2470
DarknessXShiro <darknessxshiro@gmail.com>
2015-06-30 15:07:40 +0200

    added user.txt and user data model

---
+++ Assets/Scripts/DataModel/User.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class User {
+
+	public string username {get{ return _username;} set {_username = value;}}
+	private string _username;
+
+	public User (string username) {
+		this.username = username;
+	}
+}
