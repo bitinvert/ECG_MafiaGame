208df9c6aa22a88dd1e0d986dda1b32a57597bfe
Chelinka <kasumi5@gmx.de>
2015-06-30 22:05:07 +0200

    SearchingForOpponentText (animated)

--- Assets/Scripts/Menu/MissionChoice.cs
+++ Assets/Scripts/Menu/MissionChoice.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MissionChoice : MonoBehaviour {
 
 	Client client;
+
+	public InputField messageField;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +19,8 @@ public class MissionChoice : MonoBehaviour {
 
 		if (client != null) {
 			client.CreateMission (missionName);
+			this.CheckMissionStatus();
+
 		} else {
 			Debug.Log ("no client");
 		}
@@ -26,4 +31,38 @@ public class MissionChoice : MonoBehaviour {
 		client.JoinMission (missionName);
 	}
 
+	private void CheckMissionStatus () 
+	{
+		StartCoroutine(checkStatus());
+	}
+
+	IEnumerator checkStatus() 
+	{
+		int i = 0;
+
+		while (true) {
+
+			switch(i) {
+				case 0: 
+					messageField.text = "searching for opponent";
+					break;
+				case 1:
+					messageField.text = "searching for opponent.";
+					break;
+				case 2:
+					messageField.text = "searching for opponent..";
+					break;
+				case 3:
+					messageField.text = "searching for opponent...";
+					break;
+				case 4:
+					i = 0;
+					break;
+			}
+
+			i++;
+
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
 }
