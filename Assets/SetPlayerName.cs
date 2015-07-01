cc532eab2ade32a0bf9053c4165745e4bd17923a
Chelinka <kasumi5@gmx.de>
2015-07-01 23:51:52 +0200

    Menu Stuff
    - added setPlayerName to show the player's name in the MissionOverlay
    - added several pictures
    - changed several pictures
    - changed lifebar appearance
    - created prettier dummy menus

---
+++ Assets/SetPlayerName.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetPlayerName : MonoBehaviour {
+
+	public Text nameField;
+	private GameObject client;
+	private bool nameSet;
+
+	// Use this for initialization
+	void Start () {
+		client = GameObject.FindGameObjectWithTag ("Client");
+		nameSet = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((client == null) && (!nameSet)) {
+
+			client = GameObject.FindGameObjectWithTag ("Client");
+
+		} else if(!nameSet)
+		{
+			Client c = client.GetComponent<Client>();
+			nameField.text = c.user.username;
+			nameSet = true;
+		}
+	}
+}
