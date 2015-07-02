b5cae0a9bd69859e8e423d7ec993e4c6560972e3
Chelinka <kasumi5@gmx.de>
2015-07-02 18:39:05 +0200

    More Menu Stuff
    - Profile Screen has now some functionalities

---
+++ Assets/SetFractionAvatar.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetFractionAvatar : MonoBehaviour {
+
+	public GameObject[] police_avatar = new GameObject[2];
+	public GameObject[] mafia_avatar = new GameObject[2];
+	private GameObject client;
+	private bool avatarSet;
+
+	// Use this for initialization
+	void Start () {
+		client = GameObject.FindGameObjectWithTag ("Client");
+		avatarSet = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((client == null) && (!avatarSet)) {
+			
+			client = GameObject.FindGameObjectWithTag ("Client");
+
+		} else if(!avatarSet)
+		{
+			Client c = client.GetComponent<Client>();
+
+			if(c.IsMafia) 
+			{
+				mafia_avatar[0].SetActive(true);
+				mafia_avatar[1].SetActive(true);
+
+			} else if(c.IsPolice) 
+			{
+				police_avatar[0].SetActive(true);
+				police_avatar[1].SetActive(true);
+			}
+
+			avatarSet = true;
+
+		}	
+	}
+}
