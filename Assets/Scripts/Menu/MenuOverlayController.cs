80fb30db420ef58f0e75761f0753a559795602d3
Chelinka <kasumi5@gmx.de>
2015-07-04 22:55:49 +0200

    MenuOverlay Stuff
    - Added InventoryScreen (MockUp)
    - Added missing Avatars
    - Attack and HealButton change depending on active Unit

---
+++ Assets/Scripts/Menu/MenuOverlayController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuOverlayController : MonoBehaviour {
+
+	public GameObject attackButton;
+	public GameObject healButton;
+	public PlayerController pc;
+	
+	// Update is called once per frame
+	void Update () {
+		CheckMedic (pc.pUnitActive);
+	}
+
+	private void CheckMedic(Unit u) 
+	{
+		if (u.GetType () == typeof(Medic)) 
+		{
+			healButton.SetActive (true);
+			attackButton.SetActive (false);
+
+		} else 
+		{
+			healButton.SetActive (false);
+			attackButton.SetActive (true);
+		}
+	}
+}
