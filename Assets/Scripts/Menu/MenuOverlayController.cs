7530a5616f6bc7731d3c3a2abe0d738e23f1a17f
Chelinka <kasumi5@gmx.de>
2015-07-07 16:48:34 +0200

    Added Visible OwnTurn (will be exchanged later)
    Auf Deutsch: Man sieht jetzt, ob man am Zug ist oder nicht. Der
    EndTurnButton wird entsprechend ausgegraut und disabled.
    Wird noch durch eine hübschere Grafik ersetzt.

--- Assets/Scripts/Menu/MenuOverlayController.cs
+++ Assets/Scripts/Menu/MenuOverlayController.cs
@@ -1,16 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MenuOverlayController : MonoBehaviour {
 
 	public GameObject attackButton;
 	public GameObject healButton;
+
+	public GameObject endTurnButton;
+
 	public PlayerController pc;
-	
+
+	private Client mClientPlayer;
+
+	void Start() {
+		mClientPlayer = Object.FindObjectOfType(typeof(Client)) as Client;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if(pc.pUnitActive != null){
 			CheckMedic (pc.pUnitActive);
+		}
+
+		if(!mClientPlayer.IsMyTurn) {
+			endTurnButton.GetComponent<Button>().interactable = false;
+		} else {
+			endTurnButton.GetComponent<Button>().interactable = true;
 		}
 	}
 
